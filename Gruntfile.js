d09e019f9a75f2d9d1f030cd6ddc461b3ac49e70
galyjkk <galyjkk@126.com>
2015-03-22 00:16:24 +0800

    test

--- Gruntfile.js
+++ Gruntfile.js
@@ -73,7 +73,7 @@ module.exports = function (grunt) {
       options: {
         port: 7000,
         // Change this to '0.0.0.0' to access the server from outside.
-        hostname: 'localhost',
+        hostname: '0.0.0.0',
         livereload: 35727
       },
       livereload: {
